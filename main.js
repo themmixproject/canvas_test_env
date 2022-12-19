2b707961fc54875841e9a20f4f9dba1311a201db
themmixproject <steven.devalk@gmail.com>
2022-12-19 22:43:42 +0100

    Added device pixel ratio code

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 // setting up canvas
 var canvas = document.getElementById("canvas");
 var canvasContext = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = 500
+
 // canvas.height = window.innerHeight;
 
 let output = document.getElementById("output");
 console.log(output)
-
-var canvasCenter = {
-    x: canvas.width / 2,
-    y: canvas.height / 2
-};
 
 var devicePixelRatio = window.devicePixelRatio || 1;
 var backingStoreRatio = canvasContext.webkitBackingStorePixelRatio ||
@@ -24,7 +18,14 @@ devicePixelRatio = devicePixelRatio / backingStoreRatio;
 output.innerHTML = devicePixelRatio;
 
 
+let height = 500; 
+canvas.height =height * devicePixelRatio;
+canvas.style.height = height + "px"
 
+let width = window.innerWidth;
+canvas.width = width * devicePixelRatio;
+canvas.style.width  = width + "px"
+canvasContext.setTransform(devicePixelRatio, 0, 0, devicePixelRatio, 0, 0)
 
 canvasContext.lineWidth = 5
 canvasContext.beginPath();
