5bc9916976ae78a159a3f9d934bddc853346bf34
themmixproject <steven.devalk@gmail.com>
2022-12-19 22:52:44 +0100

    Added old version of files

---
+++ old/main.js
@@ -0,0 +1,41 @@
+// // setting up canvas
+var canvas = document.getElementById("canvas");
+var canvasContext = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = 500
+// canvas.height = window.innerHeight;
+
+let output = document.getElementById("output");
+console.log(output)
+
+
+var devicePixelRatio = window.devicePixelRatio || 1;
+var backingStoreRatio = canvasContext.webkitBackingStorePixelRatio ||
+canvasContext.mozBackingStorePixelRatio ||
+canvasContext.msBackingStorePixelRatio ||
+canvasContext.oBackingStorePixelRatio ||
+canvasContext.backingStorePixelRatio || 1;
+
+devicePixelRatio = devicePixelRatio / backingStoreRatio;
+output.innerHTML = devicePixelRatio;
+
+
+
+
+canvasContext.lineWidth = 5
+canvasContext.beginPath();
+canvasContext.arc(100, 75, 50, 0, 2 * Math.PI);
+canvasContext.stroke();
+
+canvasContext.lineCap = "round"
+canvasContext.lineWidth = 10
+
+canvasContext.beginPath();
+canvasContext.moveTo(100, 300);
+canvasContext.lineTo(300, 150);
+canvasContext.stroke();
+
+
+
+
+
